328b1ad7a12436176edcde14a2dd05d429ff0f2f
Jamie Blondin <jblondin@gmail.com>
2017-11-24 12:55:43 -0500

    Updated type inference method

--- src/inference.rs
+++ src/inference.rs
@@ -9,22 +9,42 @@ pub trait InferPromotion: ::std::marker::Sized {
     fn infer_promotion(&self, dest_ty: Self) -> Option<Self>;
 }
 
+/// Container for inferred operand / result types for unary operations.
+pub struct BinaryOpTypes<T> {
+    /// Operation result type.
+    pub result: T,
+    /// Required left operand type.
+    pub left: T,
+    /// Required right operand type.
+    pub right: T,
+}
+
 /// Trait for determining the result type of a binary operation.
-pub trait InferResultBinary {
+pub trait InferTypesBinary {
     /// The Type object being used for the operands.
     type Operand: Type;
 
-    /// Returns `Some(...)` containing the resulting Type when the operation is possible, and `None`
-    /// when the operation is not possible with the given operands.
-    fn infer_result_type(&self, left: Self::Operand, right: Self::Operand) -> Option<Self::Operand>;
+    /// When the operation is possible, returns `Some(...)` containing the required operand
+    /// coercions and result type.
+    /// When the operation is not possible with the given operand types, returns `None`.
+    fn infer_types(&self, left: Self::Operand, right: Self::Operand)
+        -> Option<BinaryOpTypes<Self::Operand>>;
 }
 
+/// Container for inferred operand / result types for unary operations.
+pub struct UnaryOpTypes<T> {
+    /// Operation result type.
+    pub result: T,
+    /// Required operand type.
+    pub operand: T,
+}
 /// Trait for determining the result type of a unary operation.
-pub trait InferResultUnary {
+pub trait InferTypesUnary {
     /// The Type object being used as the operand.
     type Operand: Type;
 
-    /// Returns `Some(...)` containing the resulting Type when the operation is possible, and `None`
-    /// when the operation is not possible with the given operand.
-    fn infer_result_type(&self, operand: Self::Operand) -> Option<Self::Operand>;
+    /// When the operation is posssible, returns `Some(...)` containing the required operand
+    /// coercions and the result type.
+    /// When the operation is not possible with the given operand type, return `None`.
+    fn infer_types(&self, operand: Self::Operand) -> Option<UnaryOpTypes<Self::Operand>>;
 }
